8b5cd2e76fe9fcc7cc69b06514c8eb367455e6f7
ggyupi <beryl32451@gmail.com>
2023-10-23 17:06:54 +0700

    modifikasi percobaan 3 nomer 1

--- ArrayRataNilai05.java
+++ ArrayRataNilai05.java
@@ -7,6 +7,7 @@ public class ArrayRataNilai05 {
     public static void main(String[] args) {
         Scanner input05 = new Scanner(System.in);
         int[] nilaiMahasiswa = new int[10];
+        int counter = 0;
         double total = 0;
         double rata2;
 
@@ -14,14 +15,15 @@ public class ArrayRataNilai05 {
             System.out.print("Masukkan nilai mahasiswa ke- " + (i+1) + " : ");
             nilaiMahasiswa[i] = input05.nextInt();
         }
-
-        for (int i = 0; i < nilaiMahasiswa.length; i++) {
-            total += nilaiMahasiswa[i];
-        }
-
-        rata2 = total/nilaiMahasiswa.length;
-        System.out.println("Rata-rata nilai mahasiswa adalah " +rata2);
-        input05.close();
-
+            for (int i = 0; i < nilaiMahasiswa.length; i++) {
+                total += nilaiMahasiswa[i];
+                if (nilaiMahasiswa[i] > 70) {
+                    counter++;
+                }
+            }       
+            rata2 = total/nilaiMahasiswa.length;
+            System.out.println("Rata-rata nilai mahasiswa adalah " +rata2);
+            System.out.print("Banyaknya mahasiswa yang lulus adalah " + counter + " orang ");
+            input05.close();
     }
 }
