cef7821aaf566bbaf52fbb992c83e3010b1a76fd
ggyupi <beryl32451@gmail.com>
2023-10-24 17:00:21 +0700

    modifikasi percobaan 2

--- ArrayNilai05.java
+++ ArrayNilai05.java
@@ -8,12 +8,15 @@ public class ArrayNilai05 {
         Scanner input05 = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = input05.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke- " + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
         }
         input05.close();
     }
