44404e6264a6b5d54cb1f775f1aaace18134d67d
ggyupi <beryl32451@gmail.com>
2023-10-24 16:40:47 +0700

    modifikasi percobaan 1

--- ArrayBilagan05.java
+++ ArrayBilagan05.java
@@ -1,23 +1,14 @@
-import java.util.Scanner;
 /**
  * ArrayBilagan05
  */
 public class ArrayBilagan05 {
 
     public static void main(String[] args) {
-        Scanner input05 = new Scanner(System.in);
-        int[] bil = new int [4];
-
-        bil [0] = 5;
-        bil [1] = 13;
-        bil [2] = -7;
-        bil [3] = 17;
+        int[] bil ={5, 13, -7, 17};
 
         System.out.println(bil [0]);
         System.out.println(bil [1]);
         System.out.println(bil [2]);
         System.out.println(bil [3]);
-
-        input05.close();
     }
 }
